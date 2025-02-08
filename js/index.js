5a9b42bdde01942c5bf3bc1531930699094f6553
shahadot99999 <shahadot99999@gmail.com>
2025-02-08 19:05:49 +0600

    Debug API, Network tab,Status code, headers, bad API

--- js/index.js
+++ js/index.js
@@ -22,3 +22,46 @@ function displayUsers2(data){
         ul.appendChild(li);
     }
 }
+
+
+//Deleting a resource
+
+function deleteAPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+    });
+    
+}
+
+
+//Patching a resource
+function patchAPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PATCH',
+        body: JSON.stringify({
+          title: 'foo',
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+//Creating a resource
+function createAPost(){
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
